0cd30d33de15c53a94b5ee71f66931fec7e1557f
Cyril26 <cyrilodm26@gmail.com>
2023-10-05 19:54:19 +0000

    load balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,18 +1,14 @@
-utomate the task of creating a custom HTTP header response with Puppet.
-# The name of the custom HTTP header must be X-Served-By.
-# The value of the custom HTTP header must be the hostname of the server Nginx is running on.
+# Custom HTTP header in a nginx server
 
-exec {'update':
-  command => '/usr/bin/apt-get update',
+# update ubuntu server
+exec { 'update server':
+  command  => 'apt-get update',
+  user     => 'root',
+  provider => 'shell',
 }
--> package {'nginx':
-  ensure => 'present',
-}
--> file_line { 'http_header':
-  path  => '/etc/nginx/nginx.conf',
-  line  => "http {\n\tadd_header X-Served-By \"${hostname}\";",
-  match => 'http {',
-}
--> exec {'start':
-  command => '/usr/sbin/service nginx start',
+->
+# install nginx web server on server
+package { 'nginx':
+  ensure   => present,
+  provider => 'apt'
 }
